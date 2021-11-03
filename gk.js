2bfe24c5a478390f027119c47476e2403332e246
Dhruvil Hirpara <89528648+Dhruvil100@users.noreply.github.com>
2021-11-03 23:51:07 +0530

    Add files via upload

---
+++ gk.js
@@ -0,0 +1,137 @@
+// CREATE A QUIZ CLASS
+class Quiz {
+    constructor(questions) {
+        this.score = 0;
+        this.questions = questions;
+        this.questionIndex = 0;
+    }
+
+    getQuestionIndex() {
+        return this.questions[this.questionIndex];
+    }
+
+    guess(answer) {
+        if (this.getQuestionIndex().isCorrectAnswer(answer)) {
+            this.score += 10;
+        }
+        this.questionIndex++;
+    }
+
+    isEnded() {
+        return this.questionIndex === this.questions.length;
+    }
+}
+
+// Create a question Class
+class Question {
+    constructor(text, choices, answer) {
+        this.text = text;
+        this.choices = choices;
+        this.answer = answer;
+    }
+
+    isCorrectAnswer(choice) {
+        return this.answer === choice;
+    }
+}
+
+// NOW DISPLAY THE QUESTIONS
+function displayQuestion() {
+    if (quiz.isEnded()) {
+        showScores();
+    } else {
+        // show question
+        let questionElement = document.getElementById("question");
+        questionElement.innerHTML = quiz.getQuestionIndex().text;
+
+        // show options
+        let choices = quiz.getQuestionIndex().choices;
+        for (let i = 0; i < choices.length; i++) {
+            let choiceElement = document.getElementById("choice" + i);
+            choiceElement.innerHTML = choices[i];
+            guess("btn" + i, choices[i]);
+        }
+
+        showProgress();
+    }
+};
+
+// GUESS ANSWER
+function guess(id, guess) {
+    let button = document.getElementById(id);
+    button.onclick = function() {
+        quiz.guess(guess);
+        displayQuestion();
+    }
+};
+
+// SHOW QUIZ PROGRESS
+function showProgress() {
+    let currentQuestionNumber = quiz.questionIndex + 1;
+    let ProgressElement = document.getElementById("progress");
+    ProgressElement.innerHTML =
+        `Question ${currentQuestionNumber} of ${quiz.questions.length}`;
+};
+
+// SHOW SCORES
+function showScores() {
+    let quizEndHTML =
+        `
+    <h1>Quiz Completed</h1>
+    <h2 id='score'> You scored: ${quiz.score} of ${quiz.questions.length * 10}</h2>
+    <div class="quiz-repeat">
+        <a href="index.html">Take Quiz Again</a>
+    </div>
+    `;
+    let quizElement = document.getElementById("quiz");
+    quizElement.innerHTML = quizEndHTML;
+};
+
+// create questions here
+let questions = [
+    new Question(
+        "Anime Orginated in which country?", ["Singapore", "Japan", "South Korea", "North Korea"], "Japan"
+    ),
+    new Question(
+        "Which of the following countries are not in Europe?", ["Bulgaria", "Ukraine", "Armenia", "Belarus"], "Armenia"
+    ),
+    new Question(
+        "Facebook changed it's name to Meta to show their emphasis on metaverse. Metaverse is primarily LinkedIn to which of the following technologies?", ["Augumented and Virtual Reality", "Artificial Intelligence", " Blockchain", "Automation"], "Augumented and Virtual Reality"
+    ),
+    new Question(
+        "What is the name of the Greek Godess of Earth?", ["Athena", "Hera", "Aphrodite", "Gaea"], "Gaea"
+    ),
+    new Question(
+        "Which company recently became the world's most valuable public company?", ["Apple", "Microsoft", "Amazon", "Meta"], "Microsoft"
+    )
+];
+
+// INSTANTIATE quiz
+let quiz = new Quiz(questions);
+
+// display questions
+displayQuestion();
+
+
+// Add A CountDown for the Quiz
+let time = 10;
+let quizTimeInMinutes = time * 60 * 60;
+let quizTime = quizTimeInMinutes / 60;
+
+let counting = document.getElementById("count-down");
+
+function startCountdown() {
+    let quizTimer = setInterval(function() {
+        if (quizTime <= 0) {
+            clearInterval(quizTimer);
+            showScores();
+        } else {
+            quizTime--;
+            let sec = Math.floor(quizTime % 60);
+            let min = Math.floor(quizTime / 60) % 60;
+            counting.innerHTML = `TIME: ${min} : ${sec}`;
+        }
+    }, 1000);
+}
+
+startCountdown();
